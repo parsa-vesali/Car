e4365a9be224b554a19a1ea70c4b7181d0941cf0
Parsa Vesali <parsavesali84@gmail.com>
2024-02-03 10:26:39 +0330

    slider created

--- src/Components/Header/Heder.jsx
+++ src/Components/Header/Heder.jsx
@@ -1,7 +1,43 @@
-import React from 'react'
-import './Header.css'
+import "./Header.css";
+import React, { useRef, useState } from "react";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+
+// import required modules
+import { Autoplay, Pagination, EffectFade } from "swiper/modules";
 export default function Heder() {
   return (
-    <div>Heder</div>
-  )
+    <div className="swiper-container">
+      <Swiper
+        effect={"fade"}
+        pagination={{
+          dynamicBullets: true,
+        }}
+        autoplay={{
+          delay: 1500,
+          disableOnInteraction: false,
+        }}
+        loop={true}
+        modules={[Autoplay, Pagination, EffectFade]}
+        className="mySwiper"
+      >
+        <SwiperSlide className="swiper-slide"> 
+        <img src="./img/slider-2.jpg" alt="" className="slider__img" />
+        </SwiperSlide>
+        <SwiperSlide className="swiper-slide"> 
+        <img src="./img/slider-3.jpg" alt="" className="slider__img" />
+        </SwiperSlide>
+        <SwiperSlide className="swiper-slide"> 
+        <img src="./img/slider-4.jpg" alt="" className="slider__img" />
+        </SwiperSlide>
+        <SwiperSlide className="swiper-slide"> 
+        <img src="./img/slider-5.jpg" alt="" className="slider__img" />
+        </SwiperSlide>
+      </Swiper>
+    </div>
+  );
 }
