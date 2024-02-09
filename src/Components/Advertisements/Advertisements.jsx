986a400fb894abe47cf511a87f6f03e626632aa6
Parsa Vesali <parsavesali84@gmail.com>
2024-02-09 14:25:28 +0330

    car section updated

--- src/Components/Advertisements/Advertisements.jsx
+++ src/Components/Advertisements/Advertisements.jsx
@@ -7,7 +7,6 @@ import "./Advertisements.css";
 
 export default function Advertisements({ posts }) {
   const [postDatas , setPostDatas] = useState(posts)
-  console.log(postDatas);
   return (
     <>
       <div className="advertisements">
@@ -63,48 +62,52 @@ export default function Advertisements({ posts }) {
       </div>
 
       <div className="advertisements__mobile">
-          <div className="advertisements__box-mobile">
+        {
+          posts.map(post => (
+            <div className="advertisements__box-mobile">
             <div className="advertisements__box-mobile-right">
               <div className="advertisements__box-text">
                 <div className="advertisements__box-titles">
-                  <h2 className="advertisements__box-title"></h2>
-                  <p className="advertisements__box-title-date"></p>
+                  <h2 className="advertisements__box-title">{post.title}</h2>
+                  <p className="advertisements__box-title-date">{post.date}</p>
                 </div>
                 <div className="advertisements__box-property">
                   <span className="advertisements__box-Property-text">
-                    1
+                       {post.model}
                   </span>
                   <span className="advertisements__box-Property-text">
-                    km 
+                    km {post.km}
                   </span>
                   <span className="advertisements__box-Property-text">
-                     1
+                     {post.gear}
                   </span>
                 </div>
                 <div className="advertisements__box-seller">
                   <Stack className="Stack" direction="row" spacing={1}>
-                    {/* {post.seller === "شخصی" ? (
+                    {post.seller === "شخصی" ? (
                       <Chip className="Chip Chip-green" label="شخصی" />
                     ) : (
                       <Chip className="Chip" label="نمایشگاه" color="primary" />
-                    )} */}
+                    )}
                   </Stack>
                 </div>
                 <div className="advertisements__box-address">
                   <p className="advertisements__box-address-text">
                     <LocationOnIcon />
-                    تهران
+                    {post.address}
                   </p>
                 </div>
               </div>
             </div>
             <div className="advertisements__box-mobile-left">
               <div className="advertisements__box-img">
-                <img src='./img-post-1webp' alt="postimg" />
+                <img src={post.mainImg} alt="postimg" />
               </div>
-              <p className="advertisements__box-price-text">1000000</p>
+              <p className="advertisements__box-price-text">{post.price}</p>
             </div>
-          </div>;
+          </div>
+          ))
+        }
 
       </div>
     </>
