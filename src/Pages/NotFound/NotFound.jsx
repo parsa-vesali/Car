2db9b7a98c3515594dea17b7d5b25958f6c9de0a
Parsa Vesali <parsavesali84@gmail.com>
2024-02-10 22:32:46 +0330

    notFound page  updated

--- src/Pages/NotFound/NotFound.jsx
+++ src/Pages/NotFound/NotFound.jsx
@@ -1,7 +1,22 @@
-import React from 'react'
+import React from "react";
+import WestIcon from "@mui/icons-material/West";
+import { Link } from "react-router-dom";
+
+import "./NotFound.css";
 
 export default function NotFound() {
   return (
-    <div>NotFound</div>
-  )
+    <div className="NotFound">
+      <div className="NotFound__img">
+        <img src="./img/404.png" alt="404" />
+      </div>
+      <h2 className="NotFound__title">متاسفانه صحفه مورد نظر یافت نشد!</h2>
+      <button className="app__btn">
+        <Link to={"/"} className="link NotFound__btn">
+          بازشگت به صفحه اصلی
+          <WestIcon />
+        </Link>
+      </button>
+    </div>
+  );
 }
