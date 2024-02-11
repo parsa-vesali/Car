f6cfdfa18abcb9e41b71c9062abd5ce01a103eeb
Parsa Vesali <parsavesali84@gmail.com>
2024-02-11 19:45:44 +0330

    main post page updated

--- src/Components/Advertisements/Advertisements.jsx
+++ src/Components/Advertisements/Advertisements.jsx
@@ -72,48 +72,56 @@ export default function Advertisements({ posts }) {
 
       <div className="advertisements__mobile">
         {posts.map((post) => (
-          <div className="advertisements__box-mobile">
-            <div className="advertisements__box-mobile-right">
-              <div className="advertisements__box-text">
-                <div className="advertisements__box-titles">
-                  <h2 className="advertisements__box-title">{post.title}</h2>
-                  <p className="advertisements__box-title-date">{post.date}</p>
-                </div>
-                <div className="advertisements__box-property">
-                  <span className="advertisements__box-Property-text">
-                    {post.model}
-                  </span>
-                  <span className="advertisements__box-Property-text">
-                    km {post.km}
-                  </span>
-                  <span className="advertisements__box-Property-text">
-                    {post.gear}
-                  </span>
-                </div>
-                <div className="advertisements__box-seller">
-                  <Stack className="Stack" direction="row" spacing={1}>
-                    {post.seller === "شخصی" ? (
-                      <Chip className="Chip Chip-green" label="شخصی" />
-                    ) : (
-                      <Chip className="Chip" label="نمایشگاه" color="primary" />
-                    )}
-                  </Stack>
-                </div>
-                <div className="advertisements__box-address">
-                  <p className="advertisements__box-address-text">
-                    <LocationOnIcon />
-                    {post.address}
-                  </p>
+          <Link to={`/car/${post.id}`} className="link">
+            <div className="advertisements__box-mobile">
+              <div className="advertisements__box-mobile-right">
+                <div className="advertisements__box-text">
+                  <div className="advertisements__box-titles">
+                    <h2 className="advertisements__box-title">{post.title}</h2>
+                    <p className="advertisements__box-title-date">
+                      {post.date}
+                    </p>
+                  </div>
+                  <div className="advertisements__box-property">
+                    <span className="advertisements__box-Property-text">
+                      {post.model}
+                    </span>
+                    <span className="advertisements__box-Property-text">
+                      km {post.km}
+                    </span>
+                    <span className="advertisements__box-Property-text">
+                      {post.gear}
+                    </span>
+                  </div>
+                  <div className="advertisements__box-seller">
+                    <Stack className="Stack" direction="row" spacing={1}>
+                      {post.seller === "شخصی" ? (
+                        <Chip className="Chip Chip-green" label="شخصی" />
+                      ) : (
+                        <Chip
+                          className="Chip"
+                          label="نمایشگاه"
+                          color="primary"
+                        />
+                      )}
+                    </Stack>
+                  </div>
+                  <div className="advertisements__box-address">
+                    <p className="advertisements__box-address-text">
+                      <LocationOnIcon />
+                      {post.address}
+                    </p>
+                  </div>
                 </div>
               </div>
-            </div>
-            <div className="advertisements__box-mobile-left">
-              <div className="advertisements__box-img">
-                <img src={post.mainImg} alt="postimg" />
+              <div className="advertisements__box-mobile-left">
+                <div className="advertisements__box-img">
+                  <img src={post.mainImg} alt="postimg" />
+                </div>
+                <p className="advertisements__box-price-text">{post.price}</p>
               </div>
-              <p className="advertisements__box-price-text">{post.price}</p>
             </div>
-          </div>
+          </Link>
         ))}
       </div>
     </>
--- src/Pages/MainPost/MainPost.jsx
+++ src/Pages/MainPost/MainPost.jsx
@@ -1,11 +1,132 @@
-import React from 'react'
-import './MainPost.css'
-import { useParams } from 'react-router-dom'
+import React from "react";
+import NavBar from "../../Components/common/NavBar/NavBar";
+import Breadcrumb from "../../Components/common/Breadcrumb/Breadcrumb";
+import ShareIcon from "@mui/icons-material/Share";
+import BookmarkAddIcon from "@mui/icons-material/BookmarkAdd";
+import LocationOnIcon from "@mui/icons-material/LocationOn";
+import PhoneIcon from "@mui/icons-material/Phone";
+import WestIcon from "@mui/icons-material/West";
+import { GiHomeGarage } from "react-icons/gi";
+import { TbManualGearbox } from "react-icons/tb";
+import { BsFillFuelPumpFill } from "react-icons/bs";
+import { IoSpeedometerOutline } from "react-icons/io5";
+import { TbSteeringWheel } from "react-icons/tb";
+import { LiaBrushSolid } from "react-icons/lia";
+import { IoCarSportOutline } from "react-icons/io5";
+
+import { useParams } from "react-router-dom";
+import "./MainPost.css";
 
 export default function MainPost() {
-    let params = useParams()
-    console.log(params.postId);
+  let params = useParams();
+
   return (
-    <div>MainPost</div>
-  )
+    <>
+      <NavBar />
+      <div className="container">
+        <Breadcrumb page={"خرید خودرو"} />
+        <div className="MainPost">
+          <div className="MainPost__right">
+            <div className="MainPost__header">
+              <h2 className="MainPost__title">بنز، کلاس CLS</h2>
+              <span className="MainPost__header-icons">
+                <ShareIcon />
+                <BookmarkAddIcon />
+              </span>
+            </div>
+            <div className="MainPost__Property">
+              <p className="MainPost__Property-text">2013</p>
+              <p className="MainPost__Property-text">CLS500</p>
+            </div>
+            <p className="MainPost__date">6 روز پیش</p>
+            <div className="MainPost__price-address">
+              <p className="MainPost__address">
+                تهران / ولنجک
+                <LocationOnIcon />
+              </p>
+              <p className="MainPost__price">توافقی</p>
+            </div>
+
+            <button className="app__btn MainPost__btn">
+              <PhoneIcon />
+              تماس با فروشنده
+            </button>
+
+            <div className="MainPost__seller">
+              <div className="MainPost__seller-right">
+                <h2 className="MainPost__seller-title">
+                  اتو گالری نیک نژاد VIP
+                </h2>
+                <p className="MainPost__seller-subtitle">
+                  تهران، ولنجک، نبش خیابان چهاردهم -پلا..
+                </p>
+                <a href="#" className="MainPost__seller-link">
+                  20 آگهی از این نمایشگاه
+                  <WestIcon />
+                </a>
+              </div>
+              <div className="MainPost__seller-left">
+                <span className="MainPost__seller-icon">
+                  <GiHomeGarage />
+                </span>
+              </div>
+            </div>
+
+            <div className="MainPost__information">
+              <div className="MainPost__information-box">
+                <span className="MainPost__information-icon">
+                  <TbManualGearbox />
+                </span>
+                <p className="MainPost__information-title">گیربکس </p>
+                <p className="MainPost__information-text">اتوماتیک</p>
+              </div>
+
+              <div className="MainPost__information-box">
+                <span className="MainPost__information-icon">
+                  <BsFillFuelPumpFill />
+                </span>
+                <p className="MainPost__information-title">نوع سوخت </p>
+                <p className="MainPost__information-text">بنزینی</p>
+              </div>
+
+              <div className="MainPost__information-box">
+                <span className="MainPost__information-icon">
+                  <IoSpeedometerOutline />
+                </span>
+                <p className="MainPost__information-title">کارکرد</p>
+                <p className="MainPost__information-text">12,000 km</p>
+              </div>
+
+              <div className="MainPost__information-box">
+                <span className="MainPost__information-icon">
+                  <TbSteeringWheel />
+                </span>
+                <p className="MainPost__information-title">رنگ داخلی</p>
+                <p className="MainPost__information-text">داخل قرمز</p>
+              </div>
+
+              <div className="MainPost__information-box">
+                <span className="MainPost__information-icon">
+                  <LiaBrushSolid />
+                </span>
+                <p className="MainPost__information-title">رنگ بدنه</p>
+                <p className="MainPost__information-text">سرمه ای</p>
+              </div>
+
+              <div className="MainPost__information-box">
+                <span className="MainPost__information-icon">
+                  <IoCarSportOutline />
+                </span>
+                <p className="MainPost__information-title">وضعیت بدنه</p>
+                <p className="MainPost__information-text">بدون رنگ</p>
+              </div>
+            </div>
+
+
+          </div>
+          <div className="MainPost__left"></div>
+        </div>
+      </div>
+    </>
+  );
 }
